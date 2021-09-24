a5bc67a4a1d55ddcd23cbc23d3bfb65aa48f0029
Harry.H.Liu <Harry.H.Liu@YZWB0295>
2021-09-24 11:03:07 +0800

    修改自启动

--- LHLStart/App.xaml.cs
+++ LHLStart/App.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +16,55 @@ namespace LHLStart
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Process instance = RunningInstance();
+            if (instance == null)
+            {
+                
+            }
+            else
+            {
+                //1.2 已经有一个实例在运行
+                HandleRunningInstance(instance);
+                Shutdown();
+            }
+        }
+
+        private static Process RunningInstance()
+        {
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            //遍历与当前进程名称相同的进程列表
+            foreach (Process process in processes)
+            {
+                //如果实例已经存在则忽略当前进程
+                if (process.Id != current.Id)
+                {
+                    //保证要打开的进程同已经存在的进程来自同一文件路径
+                    if (Assembly.GetExecutingAssembly().Location.Replace("/", @"\") == current.MainModule.FileName)
+                    {
+                        //返回已经存在的进程
+                        return process;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static void HandleRunningInstance(Process instance)
+        {
+            MessageBox.Show("您已启动该程序，请查任务栏！", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+            ShowWindowAsync(instance.MainWindowHandle, 1); //调用api函数，正常显示窗口
+            SetForegroundWindow(instance.MainWindowHandle); //将窗口放置最前端
+        }
+
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindowAsync(System.IntPtr hWnd, int cmdShow);
+
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(System.IntPtr hWnd);
     }
 }
